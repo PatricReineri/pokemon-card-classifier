2ebf9f5521ccb86252e4bd2aa3cd69749a47982f
PatricReineri <onepat96@gmail.com>
2025-09-24 21:06:42 +0200

    Fix scheduler

--- pokemon-tcg-classifiier/card-classifier.py
+++ pokemon-tcg-classifiier/card-classifier.py
@@ -5,10 +5,12 @@ from PIL import Image, ImageFilter
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from torchvision import models
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
-from torchvision.models import efficientnet_b1, EfficientNet_B1_Weights
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+
 import random
 
 class AddGaussianNoise(object):
@@ -85,38 +87,67 @@ transform =  transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+def get_data_loaders(dataset_path, batch_size=32, num_workers=4):
+    pokemon_dataset = PokemonCardDataset(root_dir=dataset_path, transform=transform)
 
-dataset_path = 'base1_images'
-print(os.path.exists(dataset_path))
-print("Dataset path:", os.path.abspath(dataset_path))
+    train_loader = DataLoader(pokemon_dataset, batch_size=32, shuffle=True, num_workers=6, pin_memory=True)
+    test_loader = DataLoader(pokemon_dataset, batch_size=32, shuffle=False, num_workers=6, pin_memory=True)
+    num_classes = len(pokemon_dataset.label_encoder.classes_)
+    return train_loader, test_loader, num_classes
 
-pokemon_dataset = PokemonCardDataset(root_dir=dataset_path, transform=transform)
+def get_model(model_name, num_classes, device):
+    match model_name:
+        case 'efficientnet_b0':
+            model = efficientnet_b0(weights=EfficientNet_B0_Weights.DEFAULT)
+        case 'resnet18':
+            model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        case 'efficientnet_b1':
+            model = models.efficientnet_b1(weights=models.EfficientNet_B1_Weights.DEFAULT)
+        case 'efficientnet_b2':
+            model = models.efficientnet_b2(weights=models.EfficientNet_B2_Weights.DEFAULT)
+        case 'efficientnet_b3':
+            model = models.efficientnet_b3(weights=models.EfficientNet_B3_Weights.DEFAULT)
+        case 'efficientnet_b4':
+            model = models.efficientnet_b4(weights=models.EfficientNet_B4_Weights.DEFAULT)
+        case 'efficientnet_b5':
+            model = models.efficientnet_b5(weights=models.EfficientNet_B5_Weights.DEFAULT)
+        case _:
+            raise ValueError(f"Unknown model name: {model_name}")
+        
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+    model = model.to(device)
 
-train_loader = DataLoader(pokemon_dataset, batch_size=32, shuffle=True, num_workers=6, pin_memory=True)
-test_loader = DataLoader(pokemon_dataset, batch_size=64, shuffle=False, num_workers=6, pin_memory=True)
+    model_path = "pokemon_classifier.pth"
+    if os.path.exists(model_path):
+        print(f"Found existing model file '{model_path}', loading weights...")
+        model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    else:
+        print(f"No pre-trained model found, starting training from scratch.")
+        
+    return model
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
+def setup_training(model_name):
+    
+    #Initialize Dataset and DataLoader
+    dataset_path = 'base1_dimages'
+    print(os.path.exists(dataset_path))
+    print("Dataset path:", os.path.abspath(dataset_path))
 
-
-model = efficientnet_b1(weights=EfficientNet_B1_Weights.DEFAULT)
-
-
-
-num_classes = len(pokemon_dataset.label_encoder.classes_)
-model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
-model = model.to(device)
-
-model_path = "pokemon_classifier.pth"
-if os.path.exists(model_path):
-    print(f"Found existing model file '{model_path}', loading weights...")
-    model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
-else:
-    print(f"No pre-trained model found, starting training from scratch.")
-
-criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
-optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-62)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    train_loader, test_loader, num_classes = get_data_loaders(dataset_path, batch_size=32, num_workers=6)
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    
+    #Initialize Model
+    model = get_model(model_name, num_classes, device)
+    
+    #define loss function, scheduler and optimizer
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
+    optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-62)
+    scheduler = CosineAnnealingLR(optimizer, T_max=25)
+    
+    return model, device, train_loader, test_loader, optimizer, criterion, scheduler
+ 
 
 def train(model, device, train_loader, optimizer, criterion, epoch):
     model.train()
@@ -150,17 +181,25 @@ def test(model, device, test_loader, criterion):
     
     return accuracy
 
-accuracy = 0 
 
-for epoch in range(1, 31):
-    train(model, device, train_loader, optimizer, criterion, epoch)
-    if epoch % 5 == 0:
-        accuracy = test(model, device, test_loader, criterion)
-    scheduler.step()
 
-    if accuracy >= 99.9:
-        print("Accuracy reached 99.9%, saving model...")
-        torch.save(model.state_dict(), "pokemon_classifier.pth")
-        break
+def train_model(model_name):
+    accuracy = 0 
+    model, device, train_loader, test_loader, optimizer, criterion, scheduler = setup_training(model_name)
+    for epoch in range(1, 51):
+        train(model, device, train_loader, optimizer, criterion, epoch)
+        if epoch % 5 == 0:
+            accuracy = test(model, device, test_loader, criterion)
+        scheduler.step()
 
-torch.save(model.state_dict(), "pokemon_classifier.pth")
+        if accuracy >= 99.9:
+            print("Accuracy reached 99.9%, saving model...")
+            torch.save(model.state_dict(), "pokemon_classifier.pth")
+            break
+
+    torch.save(model.state_dict(), "pokemon_classifier.pth")
+    
+    
+    
+if __name__ == '__main__':
+    train_model(model_name="efficientnet_b0")
